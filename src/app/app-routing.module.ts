47581edefa006bf588c9153cb523c6f8d1ed3ff2
KatlheenEscoto <escotokatlheen@gmail.com>
2021-04-26 14:23:34 -0600

    Add path in tsconfig and create the first components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SkeletonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
